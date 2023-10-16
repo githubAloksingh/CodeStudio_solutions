b2f49afa8d041cf56d7bfd2e68194228002ebcb6
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2023-10-16 07:16:24 +0530

    Create Left Rotate an Array by One.cpp

---
+++ Array/Left Rotate an Array by One.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+vector<int> rotateArray(vector<int>& arr, int n) {
+    // Write your code here.
+    int temp=arr[0];
+    for(int i=1;i<arr.size();i++){
+        arr[i-1]=arr[i];
+
+    }arr[n-1]=temp;
+    return arr;
+
+
+}
