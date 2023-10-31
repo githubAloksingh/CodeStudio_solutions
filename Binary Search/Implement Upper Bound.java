f990642864c5134cfa6b8b47c70039c53c5124ea
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2023-11-01 00:40:49 +0530

    Update Implement Upper Bound.java

--- Binary Search/Implement Upper Bound.java
+++ Binary Search/Implement Upper Bound.java
@@ -11,3 +11,30 @@ public class Solution {
     }
     }
 
+
+
+
+public class Solution {
+    public static int upperBound(int []arr, int x, int n){
+        // Write your code here.
+            int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+                }
+                else if (arr[mid]>x){
+                    high=mid-1;
+                    
+                } 
+            else {
+                low = mid + 1; 
+            }
+        }
+        return ans;
+    }
+}
