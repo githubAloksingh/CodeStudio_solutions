4425e674b6a30fde9ee29901298b30ac08085b43
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2023-11-01 00:39:58 +0530

    Update Implement Lower Bound.java

--- Binary Search/Implement Lower Bound.java
+++ Binary Search/Implement Lower Bound.java
@@ -10,3 +10,30 @@ public class Solution {
         return n;
     }
     }
+
+
+
+public class Solution {
+    public static int lowerBound(int []arr, int n, int x) {
+        //Write your code here
+             int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+                }
+                else if (arr[mid]>x){
+                    high=mid-1;
+                    
+                } 
+            else {
+                low = mid + 1; 
+            }
+        }
+        return ans;
+    }
+}
