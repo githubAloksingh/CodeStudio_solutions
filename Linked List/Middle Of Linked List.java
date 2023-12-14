a10270a1d59062815087380c8fa6d21741d614dd
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2023-12-14 18:25:39 +0530

    Update Middle Of Linked List.java

--- Linked List/Middle Of Linked List.java
+++ Linked List/Middle Of Linked List.java
@@ -17,3 +17,18 @@ public class Solution
             return head;
     }
 }
+//Tortoise Algorithm
+public class Solution
+{
+    public static Node findMiddle(Node head)
+    {
+        // Write your code here.
+         Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
