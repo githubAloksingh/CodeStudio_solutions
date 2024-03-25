55afcfc578fc22f3ecbd30859033913b1b99e170
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-03-25 23:36:50 +0530

    Create  Count nodes of linked list.java

---
+++ Linked List/Count nodes of linked list.java
@@ -0,0 +1,41 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution {
+    public static int length(Node head){
+        //Your code goes here
+        int count=0;   
+        Node temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            count++;
+        }
+        return count;
+    }
+}
