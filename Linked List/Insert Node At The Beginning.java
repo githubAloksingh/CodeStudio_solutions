8680619e91bb19a4093b6ef5bed2fe5d2fe3d685
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-03-25 23:18:03 +0530

    Create  Insert Node At The Beginning.java

---
+++ Linked List/Insert Node At The Beginning.java
@@ -0,0 +1,42 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node insertAtFirst(Node list, int newValue) {
+        // Write your code here
+        Node temp =new Node(newValue);
+        temp.next=list;
+        return temp;
+    }
+}
